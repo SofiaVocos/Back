104af556974dba546fc5c316104cd8a7206a85f8
SofiaVocos <vocosdomenesofia@gmail.com>
2022-09-22 23:40:32 -0300

    desafio 2

--- index.js
+++ index.js
@@ -1,56 +1,42 @@
-class Usuario {
-    constructor(nombre, apellido, libros = [], mascotas = []) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.libros = libros;
-        this.mascotas = mascotas;
-    }
+import {Contenedor} from "./desafio.js";
 
-    getFullName() {
-        return `${this.nombre} ${this.apellido}`;
-    }
+const producto = new Contenedor('productos')
 
-    addMascota(mascota) {
-        this.mascotas.push(mascota);
-    }
+producto.getAll()
+    .then((data) => console.log({
+        data
+    }))
+    .catch((error) => console.log({
+        error
+    }))
 
-    countMascotas() {
-        return this.mascotas.length;
-    }
+producto.save({
+        title: 'Lapicera',
+        price: '25',
+        thumbnail: 'https://aldina.com.ar/wp-content/uploads/2020/08/bic-cristal-trazo-fino-azul-1.jpg',
 
-    addBook(titulo, autora) {
-        this.libros.push({ titulo, autora });
-    }
+    })
+    .then((data) => console.log({
+        data
+    }))
+    .catch((error) => console.log({
+        error
+    }))
 
-    getBookNames() {
-        return this.libros.map((libro) => libro.titulo);
-    }
-}
+producto.getById(10)
+    .then((idProducto) => console.log({
+        idProducto
+    }))
+    .catch((error) => console.log({
+        error
+    }))
 
-const usuario1 = new Usuario(
-    "Sofía",
-    "Vocos",
-    [{ titulo: "23 razones", autora: "Cin Wololo" }],
-    ["gato", "perro"]
-);
+// producto.deleteById(3)
+//     .then(console.log(`eliminado con exito`))
+//     .catch((error) => console.log({
+//         error
+//     }))
 
-console.log(usuario1.getFullName());
-
-console.log({ cantidad: usuario1.countMascotas() });
-usuario1.addMascota("loro");
-
-console.log({ cantidad: usuario1.countMascotas() });
-
-usuario1.addBook("Caos", "Magali Tajes");
-
-const booksName = usuario1.getBookNames();
-console.log(booksName);
-
-const usuario2 = new Usuario(
-    "Belén",
-    "Cisneros",
-    [{ titulo: "Después del Minuto 8", autora: "Sol Iannaci" }],
-    ["tortuga", "conejo"]
-);
-
-console.log(usuario2.getBookNames());
+// producto.deleteAll()
+// .then(console.log(`Archivo eliminado con exito`))
+// .catch((error)=>console.log({error}))
